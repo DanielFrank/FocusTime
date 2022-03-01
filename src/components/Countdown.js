63b34ef34927229f07c1f662eb3b3946485541ef
Daniel Frank <dfrank@luxurypresence.com>
2022-02-28 20:15:30 -0600

    Initial

---
+++ src/components/Countdown.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
+
+const minutesToMillis = (min) => min * 1000 * 60;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
+
+export const Countdown = ({
+  minutes = 20,
+  isPaused = true,
+  onProgress,
+  onEnd,
+}) => {
+  const [millis, setMillis] = useState(null);
+
+  const countDown = () => {
+    setMillis((time) => {
+      if (time === 0) {
+        clearInterval(interval.current);
+        return time;
+      }
+      const timeLeft = time - 1000;
+      onProgress(timeLeft / minutesToMillis(minutes));
+      return timeLeft;
+    });
+  };
+
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const second = Math.floor(millis / 1000) % 60;
+
+  const interval = React.useRef(null);
+
+  useEffect(() => {
+    setMillis(minutesToMillis(minutes));
+  }, [minutes]);
+
+  useEffect(()=> {
+      onProgress(millis / minutesToMillis(minutes));
+      if (millis == 0) {
+        onEnd();
+      }
+  }, [millis])
+
+  useEffect(() => {
+    if (isPaused) {
+      if (interval.current) clearInterval(interval.current);
+      return;
+    }
+    interval.current = setInterval(countDown, 1000);
+    return () => clearInterval(interval.current);
+  }, [isPaused]);
+
+  return (
+    <Text style={styles.text}>
+      {formatTime(minute)}:{formatTime(second)}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: fontSizes.xxxl,
+    fontWeight: 'bold',
+    color: colors.white,
+    padding: spacing.lg,
+    backgroundColor: 'rgba(94,132,226,0.3)',
+  },
+});
---
+++ src/features/focus/FocusHistory.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, FlatList, SafeAreaView } from 'react-native';
+
+import { fontSizes, spacing } from '../../utils/sizes';
+import { RoundedButton } from '../../components/RoundedButton';
+
+const HistoryItem = ({ item, index }) => {
+  return (
+    <Text style={[styles.historyItem, itemColor(item.status)]}>
+      {item.subject}
+    </Text>
+  );
+};
+
+export const FocusHistory = ({ focusHistory, onClear }) => {
+  const clearHistory = () => {
+    onClear();
+  };
+
+  return (
+    <>
+      <SafeAreaView style={{ flex: 0.5, alignItems: 'center' }}>
+        {!!focusHistory.length && (
+          <>
+            <Text style={styles.title}>Things we focused on</Text>
+            <FlatList
+              style={{ flex: 1 }}
+              contentContainerStyle={{ flex: 1, alignItems: 'center' }}
+              data={focusHistory}
+              renderItem={HistoryItem}
+            />
+            <View style={styles.clearContainer}>
+              <RoundedButton
+                size={75}
+                title="Clear"
+                onPress={() => onClear()}
+              />
+            </View>
+          </>
+        )}
+      </SafeAreaView>
+    </>
+  );
+};
+
+const itemColor = (status) => ({
+  color: status > 1 ? 'red' : 'green',
+});
+
+const styles = StyleSheet.create({
+  historyItem: {
+    fontSize: fontSizes.md,
+  },
+  title: {
+    color: 'white',
+    fontSize: fontSizes.lg,
+  },
+  clearContainer: {
+    alignItems: 'center',
+    padding: spacing.md,
+  },
+});
